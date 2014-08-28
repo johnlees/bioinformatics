03779cf17e5e72632f082d1b5650c9e90419b61a
John Lees <lees.john6@gmail.com>
2014-08-28 11:29:28 +0100

    First working version of annotate_vcf_from_gff.pl

--- assembly_scripts/annotate_vcf_from_gff.pl
+++ assembly_scripts/annotate_vcf_from_gff.pl
@@ -7,6 +7,14 @@ use Bio::Tools::GFF;
 
 #Globals
 my $tmp_annotation = "annotation.tmp";
+my $annotation_header_file = "annotation_headers.tmp";
+
+# Headers for bcftools annotation
+my $annotation_headers = <<HEADER;
+##INFO=<ID=REGION_TYPE,Number=1,Type=String,Description="Type of region variant appears in">
+##INFO=<ID=ANNOT_ID,Number=1,Type=String,Description="ID of annotation variant appears in">
+##INFO=<ID=GENE,Number=1,Type=String,Description="Gene name variant appears in">
+HEADER
 
 # Usage and input files
 my $usage = "./annotate_vcf_from_gff.pl <gff_in> <vcf_file>";
@@ -32,11 +40,13 @@ elsif (!-e $vcf_file)
 else
 {
    # Open gff with bioperl, ignoring sequence at the bottom
+   # EDIT: Ignoring sequence seems to cause loop to hang, so include it for now
+   # despite overheads
    my $gff_io = Bio::Tools::GFF->new(-file => $annotations, -gff_version => 3) || die ("Could not open $annotations as gff v3: $!");
-   $gff_io->ignore_sequence(1);
+   $gff_io->ignore_sequence(0);
 
    open(ANNOT, ">$tmp_annotation") || die("Could not write to $tmp_annotation: $!\n");
-   print ANNOT "CHROM\tFROM\tTO\tTYPE\tID\tGENE\n";
+   print ANNOT "CHROM\tFROM\tTO\tREGION_TYPE\tANNOT_ID\tGENE\n";
    # Output has tab delimited header (and example lines)
    # CHROM FROM TO TYPE ID GENE
    # contig01 1319 1889 tRNA 2070227_00013 -
@@ -72,6 +82,22 @@ else
    close ANNOT;
 
    # Use bcftools to apply this annotation
+   # First add the necessary new headers
+   open (HEADERS, ">$annotation_header_file") || die("Could not write to $annotation_header_file: $!\n");
+   print HEADERS $annotation_headers;
+   close HEADERS;
+
+   # bgzip and tabix annotations file
+   system("bgzip $tmp_annotation");
+   system("tabix -s 1 -b 2 -e 3 $tmp_annotation.gz");
+
+   # Also annotate allele counts at the same time
+   my $bcftools_command = "bcftools annotate -p fill-AN-AC -a $tmp_annotation.gz -h $annotation_header_file -c CHROM,FROM,TO,REGION_TYPE,ANNOT_ID,GENE -o $vcf_file -O z $vcf_file";
+   system($bcftools_command);
+   system("bcftools index $vcf_file");
+
+   # Finally, remove temporary files
+   unlink "$tmp_annotation.gz", "$tmp_annotation.gz.tbi", $annotation_header_file;
 }
 
 exit(0);
