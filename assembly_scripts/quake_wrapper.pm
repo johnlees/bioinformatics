855b774a11f57c73dd42e156a3b90558f2fc00c4
John Lees <lees.john6@gmail.com>
2014-08-30 15:56:57 +0100

    Move quake functions into module, and add simple wrapper script to allow just quake to be run rather than whole variant calling pipeline

---
+++ assembly_scripts/quake_wrapper.pm
@@ -0,0 +1,125 @@
+#!/usr/bin/perl -w
+
+package quake_wrapper;
+
+use strict;
+use warnings;
+
+use threads;
+use File::Spec;
+
+my $quake_location = "/nfs/users/nfs_j/jl11/software/bin/quake/quake.py";
+
+# Error corrects fastq files using quake
+sub quake_error_correct($$$)
+{
+   my ($reads, $kmer_size, $threads) = @_;
+   my (%corrected_reads, %quake_symlinks);
+
+   mkdir "quake" || die("Could not create quake directory: $!\n");
+
+   # Prepare a file with the locations of the fastq file pairs for input to
+   # quake
+   my $quake_input_file_name = "quake_reads.txt";
+   open (QUAKE, ">quake/$quake_input_file_name") || die("Could not open $quake_input_file_name for writing: $!");
+
+   # Quake outputs where the read files originally were, which isn't
+   # necessarily writable. Create symlinks instead
+   my $cwd = getcwd();
+   foreach my $sample (keys %$reads)
+   {
+      foreach my $direction (keys %{$$reads{$sample}})
+      {
+         my $read_location = $$reads{$sample}{$direction};
+         my ($volume ,$directories, $file) = File::Spec->splitpath($read_location);
+
+         $quake_symlinks{$sample}{$direction} = $cwd . "/quake/$file";
+         symlink $read_location, $quake_symlinks{$sample}{$direction};
+      }
+      print QUAKE join(" ", $quake_symlinks{$sample}{"forward"} , $quake_symlinks{$sample}{"backward"}) . "\n";
+   }
+
+   close QUAKE;
+
+   # Run quake
+   my $quake_command = "cd quake && $quake_location -f $quake_input_file_name -k $kmer_size -p $threads &> quake.log";
+   system($quake_command);
+
+   # Set paths of corrected reads
+   foreach my $sample (keys %$reads)
+   {
+      foreach my $read_direction (keys %{$$reads{$sample}})
+      {
+         my ($volume ,$directories, $file) = File::Spec->splitpath($$reads{$sample}{$read_direction});
+         $file =~ m/^(.+)\.fastq$/;
+         $corrected_reads{$sample}{$read_direction} = "$cwd/quake/$1.cor.fastq";
+      }
+   }
+
+   return(\%corrected_reads);
+}
+
+# Parses the read file names and sample names from the input read file.
+# Returns reference to an array of sample names, and a hash of read
+# locations.
+sub parse_read_file($)
+{
+   my ($read_file) = @_;
+
+   my (%read_locations, %decompress);
+   my @sample_names;
+   open(READS, $read_file) || die("Could not open $read_file: $!\n");
+
+   while (my $read_pair = <READS>)
+   {
+      chomp($read_pair);
+      my ($sample_name, $forward_read, $backward_read) = split("\t", $read_pair);
+      push(@sample_names, $sample_name);
+
+      # Decompress reads if needed, spawing a thread to do so
+      if ($forward_read =~ /\.gz$/)
+      {
+         $decompress{$sample_name}{"forward"} = threads->create(\&decompress_fastq, $forward_read);
+      }
+      if ($backward_read =~ /\.gz$/)
+      {
+         $decompress{$sample_name}{"backward"} = threads->create(\&decompress_fastq, $backward_read);
+      }
+
+      # Store in hash of hashes
+      $read_locations{$sample_name}{"forward"} = $forward_read;
+      $read_locations{$sample_name}{"backward"} = $backward_read;
+   }
+
+   # Wait for all decompression threads to complete, and overwrite read
+   # locations with new fastq paths
+   foreach my $sample (keys %decompress)
+   {
+      foreach my $direction (keys %{$decompress{$sample}})
+      {
+         $read_locations{$sample}{$direction} = $decompress{$sample}{$direction}->join();
+      }
+   }
+
+   close READS;
+   return(\@sample_names, \%read_locations);
+}
+
+sub decompress_fastq($)
+{
+   my ($fastq) = @_;
+   my $decompressed_location;
+   my $cwd = getcwd();
+
+   print STDERR "Decompressing $fastq\n";
+   my ($volume ,$directories, $file) = File::Spec->splitpath($fastq);
+
+   $file =~ m/^(.+\.fastq)\.gz/;
+   $decompressed_location = "$cwd/$1";
+   system("gzip -d -c $fastq > $decompressed_location");
+
+   return($decompressed_location);
+}
+
+1;
+
---
+++ assembly_scripts/run_quake.pl
@@ -0,0 +1,67 @@
+#!/usr/bin/perl -w
+
+use strict;
+use warnings;
+
+use Getopt::Long;
+
+use quake_wrapper;
+
+my $help_message = <<HELP;
+Usage: ./run_quake.pl -r <reads_file> -k <kmer_size> -t <threads>
+
+Runs quake error correction on paired end reads provided as fastq(.gz) files.
+Options:
+   -r --reads    Tab delimited file of fastq or fastq.gz file locations.
+                 One line per sample. First column sample name, second
+                 column forward reads, third column reverse reads.
+                 It is best to give absolute paths
+   -k --kmer     The kmer size to use with quake. Set to
+                 ceil[log(200*G)/log(4)] where G is the genome length
+   -t --threads  The number of threads to allow quake to use
+
+   -h --help     This help message
+
+Output will be in cwd/quake
+HELP
+
+#
+# Main
+#
+my ($read_file, $kmer_size, $threads, $help);
+GetOptions ("kmer|k=i"  => \$kmer_size,
+            "threads|t=s" => \$threads,
+            "reads|r=s"  => \$read_file,
+            "help|h"     => \$help
+		   ) or die($help_message);
+
+# Parse input
+if (defined($help))
+{
+   print $help_message;
+}
+elsif (!defined($read_file) || !defined($kmer_size) || !defined($threads))
+{
+   print STDERR "One of the options was not defined. All are required\n";
+   print $help_message;
+}
+elsif (!-e $read_file)
+{
+   print STDERR "The file $read_file does not exist\n";
+}
+else
+{
+   # Options ok, run quake
+
+   # Get read locations in a hash
+   my($samples, $reads) = quake_wrapper::parse_reads($read_file);
+
+   # Run quake
+   quake_wrapper::quake_error_correct($reads, $kmer_size, $threads);
+
+   # Done
+   print "\nDone\n";
+}
+
+exit(0);
+
