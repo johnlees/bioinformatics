15d69cd7c16b1fab95fee4f3cb7fdf222ff9934a
John Lees <lees.john6@gmail.com>
2014-09-11 17:03:21 +0100

    Now allows use of sga instead of cortex (untested so far)

--- assembly_scripts/reference_free_variant_caller.pl
+++ assembly_scripts/reference_free_variant_caller.pl
@@ -2,13 +2,16 @@
 
 #
 # reference_free_variant_caller.pl
-# arguments:   assembly.fa
+# inputs:      assembly.fa
 #              annotation.gff
 #              forward_reads.fastq
 #              reverse_reads.fastq
 #
 # bsub this script with:
-# bsub -o variation.%J.o -e variation.%J.e -R "select[mem>1000] rusage[mem=1000]" -M1000 -n6 -R "span[hosts=1]" ./reference_free_variant_caller.pl <arguments>
+# bsub -o variation.%J.o -e variation.%J.e -R "select[mem>1000] rusage[mem=1000]" -M1000 -n4 -R "span[hosts=1]" ./reference_free_variant_caller.pl --cortex <arguments>
+#
+# or for sga
+# bsub -o variation.%J.o -e variation.%J.e -R "select[mem>6000] rusage[mem=6000]" -M6000 ./reference_free_variant_caller.pl --sga <arguments>
 
 #
 # Takes an assembly and two sets of paired reads and gets variation between
@@ -17,10 +20,16 @@
 # annotated if they fall within a gene.
 # Expected coverage for true variants is also output
 #
+# Cortex:
+#
 # Requires cortex_var (>1.0.5.21), stampy, bcftools (>v1.0), quake (>v0.3) and
 # jellyfish (>v2.0)
 # Cortex binaries compiled:   cortex_var_31_c1
 #                             cortex_var_63_c2
+#
+# SGA:
+# Tested on v0.10.1
+# Use the graph-diff command to get variants
 #
 # To create an assembly for use with the script, try running (with parameters
 # and jobids changed where appropriate):
@@ -58,6 +67,7 @@ my $cortex_wrapper = "/nfs/users/nfs_j/jl11/installations/CORTEX_release_v1.0.5.
 my $cortex_binaries = "/nfs/users/nfs_j/jl11/installations/CORTEX_release_v1.0.5.21/bin";
 my $stampy_location = "/nfs/users/nfs_j/jl11/installations/stampy-1.0.23/stampy.py";
 my $bcftools_location = "/nfs/users/nfs_j/jl11/software/bin/bcftools";
+my $sga_location = "/software/pathogen/external/apps/usr/local/bin/sga";
 
 my @required_binaries = ("cortex_var_31_c1", "cortex_var_63_c2");
 
@@ -97,12 +107,15 @@ FILTERS
 # Help and usage
 #
 my $usage_message = <<USAGE;
-Usage: ./reference_free_variant_caller.pl -a <assembly.fasta> -g <annotation.gff> -r <reads_file>
+Usage: ./reference_free_variant_caller.pl [--sga|--cortex] -a <assembly.fasta> -g <annotation.gff> -r <reads_file>
 
-Uses cortex_var to call variants between two samples without providing a reference
+Uses cortex_var or sga to call variants between two samples without providing a reference
 sequence
 
    Options
+   --cortex            Use cortex for variant calling
+   --sga               Use sga for variant calling
+
    -a, --assembly      Contigs of a de-novo assembly of one of the samples in
                        multi-fasta format. See help for more info.
    -g, --annotation    Annotation of the -a assembly in gff v3.
@@ -110,26 +123,38 @@ sequence
                        One line per sample. First column sample name, second
                        column forward reads, third column reverse reads.
                        It is best to give absolute paths
+   -o, --output        Prefix for output vcfs
+
+   -c, --med-cov       Median coverage of reads over the assembly. Required by
+                       sga, unused by cortex
+
    --no-error-correct  Don't run quake on reads. Input reads are fed directly
                        into cortex
    --separate-correct  Error correct each sample separately. Probably required
                        for simulated data
-   -o, --output        Prefix for output vcfs
+
    -h, --help          Shows more detailed help.
 
-Requires: cortex_var, bcftools, quake and jellyfish. See help for more details
+For cortex, requires: cortex_var, bcftools, quake and jellyfish.
+For sga, requires: sga
+See help for more details
 USAGE
 
 my $help_message = <<HELP;
 
+
  reference_free_variant_caller.pl
- arguments:   assembly.fa
-              annotation.gff
-              forward_reads.fastq
-              reverse_reads.fastq
+ inputs files:  assembly.fa
+                annotation.gff
+                forward_reads.fastq
+                reverse_reads.fastq
 
  bsub this script with:
- bsub -o variation.%J.o -e variation.%J.e -R "select[mem>1000] rusage[mem=1000]" -M1000 -n6 -R "span[hosts=1]" ./reference_free_variant_caller.pl <arguments>
+ bsub -o variation.%J.o -e variation.%J.e -R "select[mem>1000] rusage[mem=1000]" -M1000 -n4 -R "span[hosts=1]" ./reference_free_variant_caller.pl --cortex <arguments>
+
+ or for sga
+ bsub -o variation.%J.o -e variation.%J.e -R "select[mem>6000] rusage[mem=6000]" -M6000 -n2 -R "span[hosts=1]" ./reference_free_variant_caller.pl --sga <arguments>
+
 
  Takes an assembly and two sets of paired reads and gets variation between
  samples using denovo assembly only. Output is a vcf where variants are called
@@ -137,10 +162,24 @@ my $help_message = <<HELP;
  annotated if they fall within a gene.
  Expected coverage for true variants is also output
 
+ Cortex:
+
  Requires cortex_var (>1.0.5.21), stampy, bcftools (>v1.0), quake (>v0.3) and
  jellyfish (>v2.0)
  Cortex binaries compiled:   cortex_var_31_c1
                              cortex_var_63_c2
+
+ SGA:
+
+ Tested on v0.10.1
+ Uses the graph-diff command to get variants
+
+ The only input needed is the median coverage (from reads mapped to assembly)
+ Either map the reads back to the assembly with bwa/smalt/stampy and extract this, or an estimate
+ can be obtained from the contig headers in SPAdes output assemblies
+
+
+ Input files:
 
  To create an assembly for use with the script, try running (with parameters
  and jobids changed where appropriate):
@@ -294,9 +333,12 @@ sub create_cortex_index($)
 #****************************************************************************************#
 
 #* gets input parameters
-my ($assembly_file, $annotation_file, $read_file, $no_quake, $sep_correct, $output_prefix, $help);
+my ($assembly_file, $annotation_file, $cortex, $sga, $med_cov, $read_file, $no_quake, $sep_correct, $output_prefix, $help);
 GetOptions ("assembly|a=s"  => \$assembly_file,
             "annotation|g=s" => \$annotation_file,
+            "cortex" => \$cortex,
+            "sga" => \$sga,
+            "med_cov|c" => \$med_cov,
             "reads|r=s"  => \$read_file,
             "no-error-correct" => \$no_quake,
             "separate-correct" => \$sep_correct,
@@ -318,14 +360,31 @@ elsif (!-e $assembly_file || !-e $annotation_file || !-e $read_file)
    print STDERR "One or more specified input files do not exist\n";
    print STDERR $usage_message;
 }
-elsif (check_binaries())
+elsif (defined($cortex) && check_binaries())
 {
-   print STDERR "See compile instructions in cortex documentation";
+   print STDERR "See compile instructions in cortex documentation\n";
+}
+elsif (!(defined($sga) ^ defined($cortex)))
+{
+   # One and only one method can be used at a time (neor/!xor)
+   print STDERR "Must choose one and only one method to call variants\nChoose --cortex or --sga\n";
+}
+elsif (defined($sga) && !defined($med_cov))
+{
+   print STDERR "For sga the median coverage of reads over the assembly must be specified with -c\n";
+   print STDERR "See the help (-h) for more information on how to obtain this number\n"
 }
 else
 {
    my $cwd = getcwd();
    my ($samples, $reads) = quake_wrapper::parse_read_file($read_file, 0);
+
+   if (defined($sga))
+   {
+      # Don't error correct for sga
+      # TODO it might be a good idea to use sga's error correction?
+      $no_quake = 1;
+   }
 
    # Thread to error correct reads
    # Note this returns location of corrected reads
@@ -340,75 +399,119 @@ else
       $quake_thread = threads->create(\&quake_wrapper::quake_error_correct, $reads, $quake_kmer_size, $quake_threads, $sep_correct);
    }
 
-   # Thread to prepare reference with cortex and stampy
-   my $reference_thread = threads->create(\&prepare_reference, $assembly_file);
-
-   # Run cortex once threads have finished
-   $assembly_file = $reference_thread->join();
-
-   my $index_name;
-   if ($no_quake)
-   {
-      $index_name = create_cortex_index($reads);
-   }
-   else
-   {
-      $index_name = create_cortex_index($quake_thread->join());
-   }
-
-   my $approx_length = reference_length($assembly_file);
-
-   my $cortex_command = "perl $cortex_wrapper --first_kmer $first_kmer --last_kmer $last_kmer --kmer_step $kmer_step --fastaq_index $index_name --auto_cleaning $auto_cleaning --bc $bc --pd $pd --outdir $out_dir --outvcf $output_prefix --ploidy $ploidy --refbindir $ref_bin_dir --list_ref_fasta $ref_se --stampy_hash REF --stampy_bin $stampy_location --genome_size $approx_length --mem_height $mem_height --mem_width $mem_width --squeeze_mem --vcftools_dir $old_vcftools_location --do_union $do_union --ref $ref_usage --workflow $workflow --logfile $ctx_logfile --apply_pop_classifier";
-
-   print STDERR "Running cortex\n";
-   print STDERR "$cortex_command\n\n";
-   system("$cortex_command &> cortex.err");
-
-   # Output expected coverage for each variant type, but use correct
-   # coverage and read length
-   #
-   # TODO: This assumes no further cleaning by cortex. It might be best either
-   # just to take cortex's calculated value, or input the quality threhold to
-   # quake rather than cortex
-   my $coverage_fastq = $$reads{$$samples[0]}{"forward"};
-   print STDERR "Using reads in $coverage_fastq for expected coverage calculations\n";
-
-   # Expected coverage is 2 times this as we've only counted one end of each
-   # read
-   my $read_length = assembly_common::read_length($coverage_fastq);
-   my $expected_coverage = 2*assembly_common::expected_coverage($coverage_fastq, $approx_length);
-
-   print "Effective coverage at k=$first_kmer: " . assembly_common::effective_coverage($first_kmer, $read_length, $expected_coverage) . "\n";
-   print "Effective coverage at k=$last_kmer: " . assembly_common::effective_coverage($last_kmer, $read_length, $expected_coverage) . "\n";
-
-   # Fix vcf output
-   # TODO: This isn't ideal as some of the name is hard coded when it can be inferred
-   # from the run_calls.pl parameters
-   my $output_vcf = "$cwd/$out_dir/vcfs/$output_prefix" . "_wk_flow_J_RefCO_FINALcombined_BC_calls_at_all_k.decomp.vcf";
-   print STDERR "Cortex output vcf is: $output_vcf\n\n";
-   print STDERR "Fixing and annotating vcf\n";
-
-   # Reheader vcf with bcftools as pop filter FILTER fields not included (bug in cortex)
    my $fixed_vcf = "$cwd/$output_prefix.decomposed_calls.vcf";
    my $filtered_vcf = "$cwd/$output_prefix.filtered_calls.vcf.gz";
 
-   system($bcftools_location . " view -h $output_vcf > vcf_header.tmp 2> $bcftools_stderr_file");
-   system("head -n -1 vcf_header.tmp > vcf_header_reduced.tmp");
-   system("tail -1 vcf_header.tmp > vcf_column_headings.tmp");
+   # Cortex variant calling
+   if (defined($cortex))
+   {
+      # Thread to prepare reference with cortex and stampy
+      my $reference_thread = threads->create(\&prepare_reference, $assembly_file);
 
-   open(FILTERS, ">filters.tmp") || die("Could not write to filters.tmp: $!\n");
-   print FILTERS $filter_lines;
-   close FILTERS;
+      # Run cortex once threads have finished
+      $assembly_file = $reference_thread->join();
 
-   system("cat vcf_header_reduced.tmp filters.tmp vcf_column_headings.tmp > new_header.tmp");
-   system($bcftools_location . " reheader -h new_header.tmp $output_vcf > $fixed_vcf 2>> $bcftools_stderr_file");
-   unlink "vcf_header.tmp", "vcf_header_reduced.tmp", "vcf_column_headings.tmp", "filters.tmp", "new_header.tmp";
+      my $index_name;
+      if ($no_quake)
+      {
+         $index_name = create_cortex_index($reads);
+      }
+      else
+      {
+         $index_name = create_cortex_index($quake_thread->join());
+      }
 
-   # Fix error in filter fields introduced by population filter fields, then
-   # bgzip and index
-   system("sed -i -e 's/,PF/;PF/g' $fixed_vcf");
-   system("bgzip $fixed_vcf");
-   system($bcftools_location . " index $fixed_vcf.gz 2>> $bcftools_stderr_file");
+      my $approx_length = reference_length($assembly_file);
+
+      my $cortex_command = "perl $cortex_wrapper --first_kmer $first_kmer --last_kmer $last_kmer --kmer_step $kmer_step --fastaq_index $index_name --auto_cleaning $auto_cleaning --bc $bc --pd $pd --outdir $out_dir --outvcf $output_prefix --ploidy $ploidy --refbindir $ref_bin_dir --list_ref_fasta $ref_se --stampy_hash REF --stampy_bin $stampy_location --genome_size $approx_length --mem_height $mem_height --mem_width $mem_width --squeeze_mem --vcftools_dir $old_vcftools_location --do_union $do_union --ref $ref_usage --workflow $workflow --logfile $ctx_logfile --apply_pop_classifier";
+
+      print STDERR "Running cortex\n";
+      print STDERR "$cortex_command\n\n";
+      system("$cortex_command &> cortex.err");
+
+      # Output expected coverage for each variant type, but use correct
+      # coverage and read length
+      #
+      # TODO: This assumes no further cleaning by cortex. It might be best either
+      # just to take cortex's calculated value, or input the quality threhold to
+      # quake rather than cortex
+      my $coverage_fastq = $$reads{$$samples[0]}{"forward"};
+      print STDERR "Using reads in $coverage_fastq for expected coverage calculations\n";
+
+      # Expected coverage is 2 times this as we've only counted one end of each
+      # read
+      my $read_length = assembly_common::read_length($coverage_fastq);
+      my $expected_coverage = 2*assembly_common::expected_coverage($coverage_fastq, $approx_length);
+
+      print "Effective coverage at k=$first_kmer: " . assembly_common::effective_coverage($first_kmer, $read_length, $expected_coverage) . "\n";
+      print "Effective coverage at k=$last_kmer: " . assembly_common::effective_coverage($last_kmer, $read_length, $expected_coverage) . "\n";
+
+      # Fix vcf output
+      # TODO: This isn't ideal as some of the name is hard coded when it can be inferred
+      # from the run_calls.pl parameters
+      my $output_vcf = "$cwd/$out_dir/vcfs/$output_prefix" . "_wk_flow_J_RefCO_FINALcombined_BC_calls_at_all_k.decomp.vcf";
+      print STDERR "Cortex output vcf is: $output_vcf\n\n";
+      print STDERR "Fixing and annotating vcf\n";
+
+      # Reheader vcf with bcftools as pop filter FILTER fields not included (bug in cortex)
+      system($bcftools_location . " view -h $output_vcf > vcf_header.tmp 2> $bcftools_stderr_file");
+      system("head -n -1 vcf_header.tmp > vcf_header_reduced.tmp");
+      system("tail -1 vcf_header.tmp > vcf_column_headings.tmp");
+
+      open(FILTERS, ">filters.tmp") || die("Could not write to filters.tmp: $!\n");
+      print FILTERS $filter_lines;
+      close FILTERS;
+
+      system("cat vcf_header_reduced.tmp filters.tmp vcf_column_headings.tmp > new_header.tmp");
+      system($bcftools_location . " reheader -h new_header.tmp $output_vcf > $fixed_vcf 2>> $bcftools_stderr_file");
+      unlink "vcf_header.tmp", "vcf_header_reduced.tmp", "vcf_column_headings.tmp", "filters.tmp", "new_header.tmp";
+
+      # Fix error in filter fields introduced by population filter fields, then
+      # bgzip and index
+      system("sed -i -e 's/,PF/;PF/g' $fixed_vcf");
+      system("bgzip $fixed_vcf");
+      system($bcftools_location . " index $fixed_vcf.gz 2>> $bcftools_stderr_file");
+   }
+   # SGA variant calling
+   elsif (defined($sga))
+   {
+      # Preprocess reference - randomly permute uncalled bases, then index
+      my ($volume ,$directories, $file) = File::Spec->splitpath($assembly_file);
+      $file =~ m/^(.+)\.([^\.])$/;
+
+      my $ref_permuted = "$1.permute.$2";
+
+      system("$sga_location preprocess --permute-ambiguous -o $ref_permuted $assembly_file");
+      system("$sga_location index $ref_permuted");
+      system("$sga_location gen-ssa $ref_permuted");
+
+      # Filter and quality trim reads (takes about 30 sec, uses almost no mem,
+      # output size is roughly the two fastqs unzipped and concat)
+      #
+      # Then index (takes about 10-15 minutes, uses about 5gb mem)
+      my @indexed_samples;
+      foreach my $sample (keys %$reads)
+      {
+         my $forward_reads = $$reads{$sample}{"forward"};
+         my $reverse_reads = $$reads{$sample}{"backward"};
+
+         my $filtered_out = "$sample.filtered.fastq";
+         push(@indexed_samples, $filtered_out);
+
+         # Filter
+         system("$sga_location preprocess --pe-mode 1 -o $filtered_out $forward_reads $reverse_reads &> filter.$sample.log");
+         # Index
+         system("$sga_location index $filtered_out &> index.$sample.log");
+      }
+
+      # Call variants (takes about 10 minutes, uses a few hundred Mb mem)
+      my $sga_x = $med_cov/3;
+
+      system("$sga_location graph-diff -k 61 -x $sga_x -r $indexed_samples[0] -b $indexed_samples[1] --ref $ref_permuted -p $output_prefix");
+
+      system("bgzip $output_prefix.variant.vcf -c > $fixed_vcf.gz");
+      system("$bcftools_location index $fixed_vcf.gz");
+   }
 
    # Annotate vcf, and extract passed variant sites only
    vcf_to_gff::transfer_annotation($annotation_file, "$fixed_vcf.gz");
