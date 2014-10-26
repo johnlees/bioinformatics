2fc4b1fa95942271e94d4a42577075a1c29e94e6
John Lees <lees.john6@gmail.com>
2014-10-26 22:49:50 +0000

    Add script to format ivr-typer output run on many samples

---
+++ bacteria_scripts/ivr_format.pl
@@ -0,0 +1,70 @@
+#!/usr/bin/perl -w
+
+use strict;
+use warnings;
+
+my @columns = ("hsdM,1.1", "hsdM,1.2", "1.1,2.1", "1.1,2.2", "1.1,2.2", "1.2,2.1", "1.2,2.2", "1.2,2.2");
+
+open(SAMPLES, "../strep_pairs_lanes.txt") || die("Could not open lanes\n");
+
+while (my $sample = <SAMPLES>)
+{
+   chomp $sample;
+   $sample =~ /^(\d+_\d+)[#_](\d+)$/;
+
+   my $sample_name = "$1_$2";
+
+   if (!open(IVR, "$sample_name/ivr_mapped_allele.txt"))
+   {
+      print STDERR ("Could not open $sample_name output\n");
+   }
+   else
+   {
+      my %reads_mapped;
+      my $ivr_header = <IVR>;
+      while (my $ivr_line = <IVR>)
+      {
+         chomp $ivr_line;
+         my ($upstream, $D39, $R6, $reads) = split("\t", $ivr_line);
+
+         $reads_mapped{$upstream}{$D39} = $reads;
+      }
+
+      print $sample;
+
+      my (%max, %max_name);
+      foreach my $column (@columns)
+      {
+         my ($upstream, $map) = split(',', $column);
+
+         if (!defined($reads_mapped{$upstream}{$map}))
+         {
+            $reads_mapped{$upstream}{$map} = 0;
+         }
+         elsif (!defined($max{$upstream}) || $reads_mapped{$upstream}{$map} > $max{$upstream})
+         {
+            $max{$upstream} = $reads_mapped{$upstream}{$map};
+            $max_name{$upstream} = $map;
+         }
+
+         print "\t$reads_mapped{$upstream}{$map}";
+      }
+
+      if (defined($max_name{"hsdM"}) && defined($max_name{$max_name{"hsdM"}}))
+      {
+         print "\t" . $max_name{'hsdM'} . "-" . $max_name{$max_name{'hsdM'}} ."\n";
+      }
+      else
+      {
+         print "\t\n";
+      }
+
+      close IVR;
+   }
+
+}
+
+close SAMPLES;
+
+exit(0);
+
