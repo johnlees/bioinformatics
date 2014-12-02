a405439733b5e2ae285f99fa495c0ba17074c66c
John Lees <jl11@sanger.ac.uk>
2014-12-02 23:42:00 +0000

    Correct false positive formula, and header line

--- bacteria_scripts/batch_compare.pl
+++ bacteria_scripts/batch_compare.pl
@@ -10,22 +10,23 @@ use warnings;
 
 my @types = ("snps", "indels");
 
-print join("\t", "Run", "Type", "Power", "False positives");
+print join("\t", "Run", "Type", "Power", "False positives\n");
 
-for (my $i = 1; $i <= 100; $i++)
+for (my $i = 2; $i <= 2; $i++)
+#for (my $i = 1; $i <= 100; $i++)
 {
    chdir "R6_mutant$i";
 
    foreach my $type (@types)
    {
-      my $compare_command = "~/bioinformatics/bacteria_scripts/compare_variants.pl --vcf2 R6_mut$i.diff.vcf.gz --ref2 ~/pathogen_lustre/pairs_analysis/simulated_data/mapping/R6_mutant1/reference.renamed.fa --vcf1 ~/pathogen_lustre/simulated_bacteria/sim_pneumo/mut$i.vcf --ref1 ~/pathogen_lustre/simulated_bacteria/Streptococcus_pneumoniae_R6.fa --top-hit --type $type | grep -v \"MISMATCH\" | wc -l";
+      my $compare_command = "~/bioinformatics/bacteria_scripts/compare_variants.pl --vcf2 R6_mut$i.diff.vcf.gz --ref2 ~/pathogen_lustre/pairs_analysis/simulated_data/mapping/R6_mutant1/reference.renamed.fa --vcf1 ~/pathogen_lustre/simulated_bacteria/sim_pneumo/mut$i.vcf --ref1 ~/pathogen_lustre/simulated_bacteria/Streptococcus_pneumoniae_R6.fa --top-hit --type $type 2> /dev/null | grep -v \"MISMATCH\" | wc -l";
 
       my $true_positives = `$compare_command`;
-      my $total_positives = `bcftools view -v $type -H ~/pathogen_lustre/simulated_bacteria/sim_pneumo/mut$i.vcf 2> /dev/null | wc -l`;
+      my $real_positives = `bcftools view -v $type -H ~/pathogen_lustre/simulated_bacteria/sim_pneumo/mut$i.vcf 2> /dev/null | wc -l`;
       my $calls = `bcftools view -v $type -H R6_mut$i.diff.vcf.gz 2> /dev/null | wc -l`;
 
-      my $power = $true_positives/$total_positives;
-      my $false_positives = $calls - $total_positives;
+      my $power = $true_positives/$real_positives;
+      my $false_positives = $calls - $true_positives;
 
       print join("\t", $i, $type, $power, "$false_positives\n");
    }
