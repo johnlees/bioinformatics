93ccf8ef73bd81b6e615d09f1cfafe4c8f39d302
John Lees <lees.john6@gmail.com>
2015-01-05 13:44:46 +0000

    Add small script to convert data for R plot script

---
+++ bacteria_scripts/pneumo_ivr/ivr_to_pairs.pl
@@ -0,0 +1,42 @@
+#!/usr/bin/perl -w
+#
+use strict;
+use warnings;
+
+my $usage = "./ivr_to_pairs.pl ivr_mapped_alleles.txt pairs.txt\n";
+
+open(IVR, $ARGV[0]) || die($usage);
+
+my $i = 1;
+my %input;
+
+while (my $line_in = <IVR>)
+{
+   chomp $line_in;
+
+   my @fields = split("\t", $line_in);
+
+   $input{$fields[0]} = $i;
+
+   $i++;
+}
+
+close IVR;
+
+open(PAIRS, $ARGV[1]) || die ($usage);
+
+print "CSF_index\tBlood_index\n";
+while (my $line_in = <PAIRS>)
+{
+   chomp $line_in;
+
+   my ($sample, $csf, $blood) = split("\t", $line_in);
+
+   if (defined($input{$csf}) && defined($input{$blood}))
+   {
+      print $input{$csf} . "\t" . $input{$blood} . "\n";
+   }
+}
+
+exit(0);
+
