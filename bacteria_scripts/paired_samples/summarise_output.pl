770e2fd2070155215f72f3069ca6d11173f00b78
John Lees <lees.john6@gmail.com>
2015-01-16 12:14:34 +0000

    Add different format of gene lists

--- bacteria_scripts/paired_samples/summarise_output.pl
+++ bacteria_scripts/paired_samples/summarise_output.pl
@@ -3,11 +3,16 @@
 use strict;
 use warnings;
 
-my $pairs_file = $ARGV[0];
+my %gene_counts;
+my $mode = $ARGV[0];
+my $pairs_file = $ARGV[1];
 
 open(PAIRS, $pairs_file) || die("Could not open $pairs_file\n");
 
-print join("\t", "Sample", "SNPs", "INDELs", "Total", "Genes") . "\n";
+unless ($mode = "genes")
+{
+   print join("\t", "Sample", "SNPs", "INDELs", "Total", "Genes") . "\n";
+}
 
 my $header = <PAIRS>;
 while (my $line_in = <PAIRS>)
@@ -35,9 +40,21 @@ while (my $line_in = <PAIRS>)
          my $genes = `bcftools query -f '%GENE\n' $vcf_name`;
          my @gene_list = split("\n", $genes);
 
-         pop(@gene_list);
+         unless ($mode = "genes")
+         {
+            print join("\t", $sample, $snps, $indels, $total, @gene_list) . "\n";
+         }
 
-         print join("\t", $sample, $snps, $indels, $total, @gene_list) . "\n";
+         my %counted;
+         foreach my $gene (@gene_list)
+         {
+            unless(defined($counted{$gene}))
+            {
+               $gene_counts{$gene}++;
+               $counted{$gene} = 1;
+            }
+         }
+
       }
       else
       {
@@ -53,6 +70,17 @@ while (my $line_in = <PAIRS>)
 }
 
 close PAIRS;
+
+if ($mode eq "genes")
+{
+   print join("\t", "Gene name", "Samples with variation") . "\n";
+
+   foreach my $gene (sort keys %gene_counts)
+   {
+      print join("\t", $gene, $gene_counts{$gene}) . "\n";
+   }
+}
+
 
 exit(0);
 
