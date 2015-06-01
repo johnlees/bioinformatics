8edd9c33b652c8181f38d0b0a316141975832561
John Lees <lees.john6@gmail.com>
2015-06-01 12:44:12 +0100

    genes_to_contig.pl: Concatenates all contigs to create genome alignment file

---
+++ sequence_evolution/genes_to_contig.pl
@@ -0,0 +1,39 @@
+#!/usr/bin/perl -w
+
+use strict;
+use warnings;
+
+use Bio::SeqIO;
+
+my $input_files = $ARGV[0];
+
+open(LIST, $input_files) || die("Could not open $input_files\n");
+
+while (my $fasta_file = <LIST>)
+{
+   chomp $fasta_file;
+
+   my $contigs_in = Bio::SeqIO->new(-file => "$fasta_file",
+                                    -format => 'Fasta');
+
+   my $contig = "";
+   while (my $fasta_seq = $contigs_in->next_seq())
+   {
+      $contig .= $fasta_seq->seq();
+   }
+
+   my $contigs_out = Bio::SeqIO->new(-file => ">$fasta_file",
+                                     -format => 'Fasta');
+   my $seq_out = Bio::Seq->new(-display_id => "$fasta_file",
+                              -seq => $contig);
+
+   $contigs_out->write_seq($seq_out);
+
+   $contigs_out->close();
+}
+
+close LIST;
+
+exit(0);
+
+
