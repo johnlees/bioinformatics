6d39cd4d4789da542178feb9cd12a1c524944411
John Lees <lees.john6@gmail.com>
2015-07-15 14:50:20 +0100

    Add ALF pangwas testing scripts
    Find out good sites to use by checking MAF
    Simulate phenotype based on genotype

---
+++ sequence_evolution/ALF/count_snp_sites_maf.pl
@@ -0,0 +1,39 @@
+#!/usr/bin/perl -w
+
+# Counts MAF from vcfs produced by snp_sites
+# alignment -> snp_sites -> this script
+# snp_sites -v MSA_88_dna.fa
+# ./count_snp_sites_maf.pl MSA_88_dna.fa.vcf
+
+use strict;
+use warnings;
+
+my $vcf_in = $ARGV[0];
+
+open(VCF, $vcf_in) || die("Could not open $vcf_in\n");
+
+while (my $vcf_line = <VCF>)
+{
+   if ($vcf_line !~ /^#/)
+   {
+      chomp $vcf_line;
+
+      my ($chrom, $pos, $id, $ref, $alt, $qual, $filter, $info, $format, @vals) = split("\t", $vcf_line);
+
+      my $mac = 0;
+      foreach my $val (@vals)
+      {
+         if ($val ne ".")
+         {
+            $mac++
+         }
+      }
+
+      my $maf = $mac/scalar(@vals);
+
+      print join("\t", $pos, $ref, $alt, $maf) . "\n";
+   }
+}
+
+exit(0);
+
---
+++ sequence_evolution/ALF/create_pheno.pl
@@ -0,0 +1,76 @@
+#!/usr/bin/perl -w
+
+# Creates a pheno file with the specified case ratio and OR, given a snp
+# position
+#
+# ./create_pheno.pl msa.fa.vcf pos ratio OR
+
+use strict;
+use warnings;
+
+my $vcf_in = $ARGV[0];
+my $pos_in = $ARGV[1];
+my $ratio = $ARGV[2];
+my $or = $ARGV[3];
+
+my ($ref_prob, $alt_prob);
+if ($or > 0)
+{
+   $ref_prob = $ratio;
+   $alt_prob = $ratio * $or;
+}
+else
+{
+   $ref_prob = 0;
+   $alt_prob = 1;
+}
+
+open(VCF, $vcf_in) || die("Could not open $vcf_in\n");
+
+my @sample_names;
+while (my $vcf_line = <VCF>)
+{
+   if ($vcf_line !~ /^##/)
+   {
+      chomp $vcf_line;
+      my ($chrom, $pos, $id, $ref, $alt, $qual, $filter, $info, $format, @vals) = split("\t", $vcf_line);
+
+      if ($vcf_line =~ /^#/)
+      {
+         foreach my $sample (@vals)
+         {
+            $sample =~ m/^SE(\d+)\/(\d+)$/;
+            push(@sample_names, "SE$1");
+         }
+      }
+      elsif ($pos_in == $pos)
+      {
+         my $i = 0;
+         foreach my $sample (@sample_names)
+         {
+            my $pheno = 0;
+            if ($vals[$i] eq $ref)
+            {
+               if (rand() < $ref_prob)
+               {
+                  $pheno = 1;
+               }
+            }
+            else
+            {
+               if (rand() < $alt_prob)
+               {
+                  $pheno = 1;
+               }
+            }
+
+            print join("\t", $sample, $sample, $pheno) . "\n";
+
+            $i++;
+         }
+      }
+   }
+}
+
+exit(0);
+
