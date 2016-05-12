14485f91cb1feea783627be4e2893514226459ed
John Lees <lees.john6@gmail.com>
2016-05-12 14:19:01 +0100

    Better output naming in filter_variants

--- bacteria_scripts/filter_variants.pl
+++ bacteria_scripts/filter_variants.pl
@@ -14,11 +14,13 @@ use File::Basename;
 use lib dirname( abs_path $0 ) . "/../assembly_scripts";
 use lib dirname( abs_path $0 );
 
+use File::Copy qw(copy);
+
 use mapping;
 
 # VCF filters
 my @vcf_filters = ("FORMAT/DP < 4" , "(GT=\"0\" && PL[0]/PL[1] > 0.75) || (GT=\"1\" && PL[1]/PL[0] > 0.75)", "QUAL < 50", "MQ < 30", "PV4[*] < 0.001", "MQSB < 0.001", "RPB < 0.001");
-my @vcf_filter_names = ("DEPTH", "RATIO", "VAR_QUAL", "MAP_QUAL", "STRAND_BIAS", "MQ_BIAS", "RP_BIAS");
+my @vcf_filter_names = ("DEPTH", "RATIO", "VAR_QUAL", "MAP_QUAL", "PV4_BIAS", "MQ_BIAS", "RP_BIAS");
 
 my $usage_message = <<USAGE;
 Usage: ./filter_variants.pl -v <vcf_file>
@@ -39,7 +41,18 @@ if (defined($help) || !-e $input_vcf)
 else
 {
    # Filter variants
+   copy $input_vcf, "tmp$input_vcf";
    mapping::filter_vcf($input_vcf, \@vcf_filters, \@vcf_filter_names);
+
+   if ($input_vcf =~ /^(.+_variant)\.bcf$/)
+   {
+      rename $input_vcf, "$1.filtered.vcf.gz";
+   }
+   else
+   {
+      rename $input_vcf, "filtered.$input_vcf.vcf.gz";
+   }
+   rename "tmp$input_vcf", $input_vcf;
 }
 
 exit(0);
