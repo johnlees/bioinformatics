3d7fd67e0ef29b13ea2d568d8cffe213ee329c0a
John Lees <lees.john6@gmail.com>
2016-05-13 11:45:12 +0100

    Update filters

--- bacteria_scripts/filter_variants.pl
+++ bacteria_scripts/filter_variants.pl
@@ -17,8 +17,8 @@ use lib dirname( abs_path $0 );
 use mapping;
 
 # VCF filters
-my @vcf_filters = ("FORMAT/DP < 4" , "(GT=\"0\" && PL[0]/PL[1] > 0.75) || (GT=\"1\" && PL[1]/PL[0] > 0.75)", "QUAL < 50", "MQ < 30", "PV4[*] < 0.001", "MQSB < 0.001", "RPB < 0.001");
-my @vcf_filter_names = ("DEPTH", "RATIO", "VAR_QUAL", "MAP_QUAL", "PV4_BIAS", "MQ_BIAS", "RP_BIAS");
+my @vcf_filters = ("FORMAT/DP < 4", "IDV < 4 || IMF < 0.2", "AF1 > 0.2 && AF1 < 0.8", "QUAL < 50", "MQ < 26", "PV4[*] < 0.001", "MQSB < 0.001", "RPB < 0.001", );
+my @vcf_filter_names = ("DEPTH", "INDEL_DEPTH", "HET", "VAR_QUAL", "MAP_QUAL", "PV4_BIAS", "MQ_BIAS", "RP_BIAS");
 
 my $usage_message = <<USAGE;
 Usage: ./filter_variants.pl -v <vcf_file> -o <output>
