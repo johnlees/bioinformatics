4afc4dea6ad489c6a9649aaf346887da0550c7a3
John Lees <lees.john6@gmail.com>
2016-05-18 11:32:02 +0100

    More general mash2matrix
    Works with any sample name

--- phylogenetics/mash2matrix.pl
+++ phylogenetics/mash2matrix.pl
@@ -6,19 +6,24 @@ use warnings;
 my $dist_file = $ARGV[0];
 open(INPUT, $dist_file) || die("Could not open $dist_file\n");
 
+my %row_idx;
 my @mat_out;
+my $i = 0;
 while (my $line_in = <INPUT>)
 {
    chomp $line_in;
    my ($el1, $el2, $dist, @junk) = split("\t", $line_in);
 
-   $el1 =~ m/SE0+(\d+)_contigs\.fa$/;
-   my $row = $1 - 1;
+   if (!defined($row_idx{$el1}))
+   {
+      $row_idx{$el1} = $i++;
+   }
+   if (!defined($row_idx{$el2}))
+   {
+      $row_idx{$el2} = $i++;
+   }
 
-   $el2 =~ m/SE0+(\d+)_contigs\.fa$/;
-   my $col = $1 - 1;
-
-   $mat_out[$row][$col] = $dist;
+   $mat_out[$row_idx{$el1}][$row_idx{$el2}] = $dist;
 }
 
 for (my $i = 0; $i < scalar(@{$mat_out[0]}); $i++)
