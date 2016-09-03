ccc040aa2695b26aa2fc2c5502ff83022d47dfb8
John Lees <lees.john6@gmail.com>
2016-09-03 15:48:31 +0100

    select sample subset from a fasta file

---
+++ bacteria_scripts/fasta_filter.pl
@@ -0,0 +1,57 @@
+#!/usr/bin/perl -w
+
+# Selects only given samples from fasta file
+
+use strict;
+use warnings;
+
+my $sample_file = $ARGV[0];
+my $fasta_file = $ARGV[1];
+
+open(SAMPLES, $sample_file) || die("Could not open $sample_file: $!\n");
+my %sample_list;
+while (my $line_in = <SAMPLES>)
+{
+   chomp $line_in;
+   $sample_list{$line_in} = 1;
+}
+
+close SAMPLES;
+
+open(FASTA, $fasta_file) || die("Could not open $fasta_file: $!\n");
+my $line_in = <FASTA>;
+while (!eof(FASTA))
+{
+   chomp $line_in;
+
+   $line_in =~ m/^>(.+)$/;
+   if (!defined($sample_list{$1}))
+   {
+      while($line_in = <FASTA>)
+      {
+         if ($line_in =~ m/^>(.+)$/)
+         {
+            last;
+         }
+      }
+   }
+   else
+   {
+      while($line_in = <FASTA>)
+      {
+         if ($line_in =~ m/^>(.+)$/)
+         {
+            last;
+         }
+         else
+         {
+            print $line_in;
+         }
+      }
+   }
+}
+close FASTA;
+
+exit(0);
+
+
